d80bbaf976143c64930921427dc8a0238229614e
Keith <kotikaleksei@icloud.com>
2017-11-09 23:34:25 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { FormsModule } from '@angular/forms';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDoGraVj8MypaiRKBpcGyz1f9t9MbQkYK4",
+  authDomain: "firestore-2f160.firebaseapp.com",
+  databaseURL: "https://firestore-2f160.firebaseio.com",
+  projectId: "firestore-2f160",
+  storageBucket: "firestore-2f160.appspot.com",
+  messagingSenderId: "787772283982"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
